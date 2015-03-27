0d2fa47abc35c0ae6d103b26797e644d82cf7d64
learrsan <gaslas@hotmail.com>
2015-03-27 16:22:08 +0000

    25/03/15

---
+++ ConsultasListas/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="6f5851d6-2428-44eb-8dd1-2e7431868504" featureId="6f5851d6-2428-44eb-8dd1-2e7431868504" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="ConsultasListas Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="d4ed4167-c1f1-4558-b9e1-e6bf95ddda82" />
+  </projectItems>
+</feature>
---
+++ ConsultasListas/VisualWebPart1/VisualWebPart1.ascx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using System.Linq;
+
+namespace ConsultasListas.VisualWebPart1
+{
+    [ToolboxItemAttribute(false)]
+    public partial class VisualWebPart1 : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public VisualWebPart1()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SPSiteDataQuery query = new SPSiteDataQuery();
+
+            query.Webs = @"<Webs scope=""Recursive"">";
+            query.Lists = @"<Lists ServerTemplate=""105"">";
+            query.Query = "<Where><Eq><FieldRef Name=\"Last Name\" />" + "<value Type='Text'>" + txtApellidos.Text + "</value>" + "</Eq></Where>";
+
+            var res = SPContext.Current.Web.GetSiteData(query);
+
+            var datos = res.CreateDataReader();
+
+            lblResultado.Text = "";
+            while (datos.Read())
+
+                for (int i = 0; i < datos.FieldCount; i++)
+                {
+                    lblResultado.Text += datos.GetValue(i).ToString();
+                }
+
+            lblResultado.Text += "<br/>";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            using (var context = new TeamsiteDataContext("http://localhost"))
+            {
+                var datos = from o in context.Contactos
+                    where o.LastName.Contains(txtApellidos.Text) select o;
+                lblResultado.Text = "";
+                foreach (var dato in datos)
+                {
+                    lblResultado.Text += dato.FirstName + "<br/>";
+                }
+            }
+        }
+    }
+}
